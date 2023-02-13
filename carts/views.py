a9c9daf47a6b421b8358ed53eb3d1f3e39b7bc1d
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-02-13 14:31:16 +0200

    ecommerse website

--- carts/views.py
+++ carts/views.py
@@ -12,8 +12,9 @@ def _cart_id(request):
 
 
 def add_cart(request, product_id):
-    color = request.GET('color')
-    size = request.GET('size')
+    if request.method == 'POST':
+        color = request.POST('color')
+        size = request.POST('size')
 
     product = Product.objects.get(id=product_id)
     try:
