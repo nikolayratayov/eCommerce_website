f32d69a09b85505554b0bfc2de955cffe7626acb
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-02-15 08:37:48 +0200

    ecommerse website

--- carts/views.py
+++ carts/views.py
@@ -34,10 +34,18 @@ def add_cart(request, product_id):
 
     try:
         cart_item = CartItem.objects.get(product=product, cart=cart)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.quantity += 1
         cart_item.save()
     except CartItem.DoesNotExist:
         cart_item = CartItem.objects.create(product=product, cart=cart, quantity=1)
+        if len(product_variation) > 0:
+            cart_item.variations.clear()
+            for item in product_variation:
+                cart_item.variations.add(item)
         cart_item.save()
     return redirect('cart')
 
