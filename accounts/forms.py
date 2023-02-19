e7490f43bb4f3d0f30da18e75ced21417677f1d3
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-02-19 17:19:50 +0200

    ecommerse website

--- accounts/forms.py
+++ accounts/forms.py
@@ -3,6 +3,12 @@ from .models import Account
 
 
 class RegistrationForm(forms.ModelForm):
+    password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Enter password'
+    }))
+    confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
+        'placeholder': 'Confirm password'
+    }))
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
