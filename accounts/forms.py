6cf7c5036f4ff4d79604698d9d30c9ae22edd4f7
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-02-20 11:13:27 +0200

    ecommerse website

--- accounts/forms.py
+++ accounts/forms.py
@@ -4,7 +4,7 @@ from .models import Account
 
 class RegistrationForm(forms.ModelForm):
     password = forms.CharField(widget=forms.PasswordInput(attrs={
-        'placeholder': 'Enter password'
+        'placeholder': 'Enter password',
     }))
     confirm_password = forms.CharField(widget=forms.PasswordInput(attrs={
         'placeholder': 'Confirm password'
@@ -12,3 +12,12 @@ class RegistrationForm(forms.ModelForm):
     class Meta:
         model = Account
         fields = ['first_name', 'last_name', 'phone_number', 'email', 'password']
+
+    def __init__(self, *args, **kwargs):
+        super(RegistrationForm, self).__init__(*args, **kwargs)
+        self.fields['first_name'].widget.attrs['placeholder'] = 'Enter First Name'
+        self.fields['last_name'].widget.attrs['placeholder'] = 'Enter Last Name'
+        self.fields['phone_number'].widget.attrs['placeholder'] = 'Enter Phone number'
+        self.fields['email'].widget.attrs['placeholder'] = 'Enter Email Address'
+        for field in self.fields:
+            self.fields[field].widget.attrs['class'] = 'form-control'
