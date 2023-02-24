c2c5cbcd586eb43ec3489f59fa55dc85c41a3865
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-02-24 13:49:18 +0200

    ecommerse website

--- accounts/views.py
+++ accounts/views.py
@@ -1,6 +1,13 @@
 from django.contrib import messages, auth
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.tokens import default_token_generator
+from django.contrib.sites.shortcuts import get_current_site
+from django.core.mail import EmailMessage
 from django.shortcuts import render, redirect
+from django.template.loader import render_to_string
+from django.utils.encoding import force_bytes
+from django.utils.http import urlsafe_base64_encode, urlsafe_base64_decode
+
 from .forms import RegistrationForm
 from .models import Account
 # Create your views here.
@@ -20,6 +27,18 @@ def register(request):
             user = Account.objects.create_user(first_name=first_name, last_name=last_name, email=email, password=password, username=username)
             user.phone_number = phone_number
             user.save()
+
+            current_site = get_current_site(request)
+            mail_subject = 'Please activate your account'
+            message = render_to_string('accounts/account_verification_email.html', {
+                'user': user,
+                'domain': current_site,
+                'uid': urlsafe_base64_encode(force_bytes(user.pk)),
+                'token': default_token_generator.make_token(user)
+            })
+            to_email = email
+            send_email = EmailMessage(mail_subject, message, to=[to_email])
+            send_email.send()
             messages.success(request, 'Registration successful.')
             return redirect('register')
     else:
