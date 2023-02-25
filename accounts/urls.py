db8048296acbcdd3e7876e5fed2397f83c2a7806
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-02-25 19:08:10 +0200

    ecommerse website

--- accounts/urls.py
+++ accounts/urls.py
@@ -5,4 +5,5 @@ urlpatterns = [
     path('register/', register, name='register'),
     path('login/', login, name='login'),
     path('logout/', logout, name='logout'),
+    path('activate/<uidb64>/<token>/', activate, name='activate'),
 ]
