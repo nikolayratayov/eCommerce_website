57472ec8028466e2c58e6fcd975c576311f74798
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-04-25 11:32:43 +0300

    ecommerse website

--- orders/admin.py
+++ orders/admin.py
@@ -1,6 +1,14 @@
 from django.contrib import admin
 from .models import Order, Payment, OrderProduct
 # Register your models here.
-admin.site.register(Order)
+
+
+class OrderAdmin(admin.ModelAdmin):
+    list_display = ['order_number', 'full_name', 'phone', 'email', 'city', 'order_total', 'tax', 'status', 'created_at']
+    search_fields = ['order_number', 'first_name', 'last_name', 'phone', 'email']
+    list_per_page = 20
+
+
+admin.site.register(Order, OrderAdmin)
 admin.site.register(Payment)
 admin.site.register(OrderProduct)
