51c604aa7d1d201e6eeadee91d30902f4254c6fb
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-05-04 09:30:05 +0300

    ecommerse website

--- store/views.py
+++ store/views.py
@@ -1,10 +1,12 @@
 from django.db.models import Q
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 from carts.models import CartItem
 from carts.views import _cart_id
-from .models import Product
+from .models import Product, ReviewRating
 from category.models import Category
+from .forms import ReviewForm
+from django.contrib import messages
 
 
 def store(request, category_slug=None):
@@ -55,3 +57,25 @@ def search(request):
         'product_count': products.count()
     }
     return render(request, 'store/store.html', context)
+
+
+def submit_review(request, product_id):
+    url = request.META.get('HTTP_REFERER')
+    if request.mehtod == 'POST':
+        try:
+            reviews = ReviewRating.objects.get(user__id=request.user.id, product__id=product_id)
+            form = ReviewForm(request.POST, instance=reviews)
+            form.save()
+            messages.success(request, 'Thank you! Your review has been updated!')
+            return redirect(url)
+        except ReviewRating.DoesNotExist:
+            form = ReviewForm(request.POST)
+            if form.is_valid():
+                data = ReviewRating()
+                data.subject = form.cleaned_data['subject']
+                data.rating = form.cleaned_data['rating']
+                data.review = form.cleaned_data['review']
+                data.ip = request.META.get('REMOTE_ADDR')
+                data.product_id = product_id
+                data.user_id = request.user.id
+                data.save()
