656fab08277b973a492ab463fefa1ddeb548d022
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-05-07 14:16:50 +0300

    ecommerse website

--- store/views.py
+++ store/views.py
@@ -44,10 +44,13 @@ def product_detail(request, category_slug, product_slug):
         order_product = OrderProduct.objects.filter(user=request.user, product_id=single_product.id).exists()
     except OrderProduct.DoesNotExist:
         order_product = None
+
+    reviews = ReviewRating.objects.filter(product_id=single_product.id, status=True)
     context = {
         'single_product': single_product,
         'in_cart': in_cart,
-        'order_product': order_product
+        'order_product': order_product,
+        'reviews': reviews
     }
     return render(request, 'store/product_detail.html', context)
 
