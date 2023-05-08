238ef0b26d15608e4e7cf2e0db6f1b02ab904b7c
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-05-08 13:44:37 +0300

    ecommerse website

--- store/models.py
+++ store/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.urls import reverse
 from accounts.models import Account
 from category.models import Category
+from django.db.models import Avg, Count
 
 # Create your models here.
 
@@ -25,6 +26,19 @@ class Product(models.Model):
     def __str__(self):
         return self.product_name
 
+    def average_review(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(average=Avg('rating'))
+        avg = 0
+        if reviews['average'] is not None:
+            avg = float(reviews['average'])
+        return avg
+
+    def count_review(self):
+        reviews = ReviewRating.objects.filter(product=self, status=True).aggregate(count=Count('id'))
+        count = 0
+        if reviews['count'] is not None:
+            count = int(reviews['count'])
+        return count
 
 class VariationManager(models.Manager):
     def colors(self):
