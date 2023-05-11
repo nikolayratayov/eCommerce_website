fe566b12a0c11a688d3f6e052aa5c810bbc86b75
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-05-11 16:48:34 +0300

    ecommerse website

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.contrib.auth.admin import UserAdmin
-from .models import Account
+from django.utils.html import format_html
+
+from .models import Account, UserProfile
 # Register your models here.
 
 
@@ -14,4 +16,19 @@ class AccountAdmin(UserAdmin):
     fieldsets = ()
 
 
+class UserProfileAdmin(admin.ModelAdmin):
+
+    def thumbnail(self, object):
+        if object.profile_picture:
+            return format_html(
+                '<img src="{}" width="30" style="border-radius:50%;">'.format(object.profile_picture.url))
+        return "-"
+
+    thumbnail.short_description = 'Profile Picture'
+
+    list_display = ('thumbnail', 'user', 'city', 'state', 'country')
+
+
 admin.site.register(Account, AccountAdmin)
+admin.site.register(UserProfile, UserProfileAdmin)
+
