93bb6a0c338fe5817f67d262d2f4883b28f2e483
nikolayratayov <99525124+nikolayratayov@users.noreply.github.com>
2023-05-18 07:40:23 +0300

    ecommerse website

--- store/models.py
+++ store/models.py
@@ -40,6 +40,7 @@ class Product(models.Model):
             count = int(reviews['count'])
         return count
 
+
 class VariationManager(models.Manager):
     def colors(self):
         return super(VariationManager, self).filter(variation_category='color', is_active=True)
@@ -80,3 +81,15 @@ class ReviewRating(models.Model):
 
     def __str__(self):
         return self.subject
+
+
+class ProductGallery(models.Model):
+    product = models.ForeignKey(Product, default=None, on_delete=models.CASCADE)
+    image = models.ImageField(upload_to='store/products', max_length=255)
+
+    def __str__(self):
+        return self.product.product_name
+
+    class Meta:
+        verbose_name = 'productgallery'
+        verbose_name_plural = 'product gallery'
